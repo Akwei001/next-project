6811d57182f564d416a5d89e7a7afe39b3249338
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2024-11-10 12:06:23 +0000

    business card added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
 
+import Link from "next/link"
+
 
 export default function Home() {
   return (
     <div>
       <main>
         <div>
-          <h1>Dan&apos;s Computer<br/> shop</h1>
+          <h1>Dan&apos;s Computer<br/> Repair shop</h1>
+          <address>
+            555 Gateway Lane<br/>
+            Kansas City, KS 55555
+          </address>
+          <p>Open daily: 9am to 5pm</p>
+          <Link href="tel:5555555555" className="hover:underline">
+            555-555-5555
+          </Link>
         </div>
       </main>
 
